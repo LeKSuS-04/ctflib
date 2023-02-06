8cd65f28ce9d2bdca83394f8bf910ef798ea25f8
Alexey Tarasov <tarasovion2004@gmail.com>
2023-02-06 19:46:27 +0300

    Add pydocstring to ruff

--- ctflib/crypto/xor.py
+++ ctflib/crypto/xor.py
@@ -1,16 +1,20 @@
+"""Contains `xor` function."""
+
 from itertools import cycle, zip_longest
 from typing import Iterable
 
 
 def xor(data: Iterable[int], key: Iterable[int] | int, cycle_key: bool = True) -> bytes:
-    """Xor two integer sequences
+    """
+    Xor two integer sequences.
 
     Args:
         data (Iterable[int]): data to be xored
         key (Iterable[int] | int): key to xor with
         cycle_key (bool, optional): cycle through the key. Defaults to True.
 
-    Returns:
+    Returns
+    -------
         bytes: result of xoring data with key
     """
     if isinstance(key, int):
